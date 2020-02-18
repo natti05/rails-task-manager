152d839eed24008a02b02936925e9bca0657c7d3
Natalia Takahara <nana_takahara@hotmail.com>
2020-02-18 16:27:14 +0900

    need some css

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
+# frozen_string_literal:true
+
+# abc
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :tasks
+  # get 'tasks', to: 'tasks#index', as: 'tasks'
+  # get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  # get 'tasks/:id', to: 'tasks#show', as: 'task'
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'task/:id', to: 'tasks#destroy', as: 'delete_task'
 end
--- db/migrate/20200218024133_create_tasks.rb
+++ db/migrate/20200218024133_create_tasks.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal:true
+
+# abc
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
       t.string :title
       t.text :details
       t.boolean :completed, default: false
-
 
       t.timestamps
     end
